6416206635fa556ab15e4067d1b9219ce98689a0
Alejandro Bermúdez <mejorandro@gmail.com>
2025-08-15 13:31:29 -0600

    Everything working as expected resolve issue about s dependency

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,10 +1,10 @@
-import MealItem from "./meals-item";
+import MealItem from "./meal-item";
 
 export default function MealsGrid({meals}){
     return <>
         <ul>
             {meals.map(meal => <li key={meal.id}>
-                <MealItem {...meal}
+                <MealItem {...meal} />
             </li>)}
         </ul>
     </>
