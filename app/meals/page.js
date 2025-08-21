e6559d32b073749a404fbc63376930685be62cbd
Alejandro Bermúdez <mejorandro@gmail.com>
2025-08-20 20:47:21 -0600

    Add logic to incorporate a sqllite3 db into the projec in order to load then meals from it's data

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,6 +3,7 @@ import classes from './page.module.css'
 import MealsGrid from "@/components/meals/meals-grid"
 
 export default function MealsPage(){
+    
     return (
         <>
            <header className={classes.header}>
