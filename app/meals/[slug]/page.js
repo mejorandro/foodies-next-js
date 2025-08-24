b34e5673d7425078d3a599a4ce73c291038649bc
Alejandro Bermúdez <mejorandro@gmail.com>
2025-08-23 22:27:55 -0600

    ix(meals): correct image and property display
    Resolved issues where meal images and related properties
    were not rendering properly. Now all meal details display
    as expected.

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,28 +1,29 @@
-import { getMeals } from '@/lib/meals';
+import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 import Image from 'next/image';
 
 export default function MealDetailsPage({ params }){
 
-    const meal = getMeals(params.slug);
+    const meal = getMeal(params.slug);
+    console.log(meal.title);
 
     return (
         <>
            <header className={classes.header}>
             <div className={classes.image}>
-                <Image fill/>
+                <Image src={meal.image} alt={meal.title} fill/>
             </div>
             <div className={classes.headerText}>
                 <h1>{meal.title}</h1>
                 <p className={classes.creator}>
-                    by <a href={`mailto:${'Email'}`}>NAME</a>
+                    by <a href={`mailto:${'Email'}`}>{meal.creator}</a>
                 </p>
-                <p className={classes.summary}>SUMMARY</p>
+                <p className={classes.summary}>{meal.summary}</p>
             </div>
            </header>
            <main>
             <p className={classes.instructions} dangerouslySetInnerHTML={{
-                __html: '...',
+                __html: meal.instructions,
             }}></p>
            </main>
         </>
