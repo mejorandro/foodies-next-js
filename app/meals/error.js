8d1a464c318fa53b6b62c667870a068ec029a696
Alejandro Bermúdez <mejorandro@gmail.com>
2025-08-23 20:33:38 -0600

    feat(error-handling): add client-side error display logic
    Added logic and files to handle and display errors when they occur.
    Ensured the client is prepared for client-side issues by showing
    a proper error message when an exception is thrown.

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Error({error}){
+    return (<main className="error">
+        <h1>An Error Ocurred!</h1>
+        <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+    );
+}
