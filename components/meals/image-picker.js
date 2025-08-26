566560470442812b03d5047566d82ef8c027afcc
Alejandro Bermúdez <mejorandro@gmail.com>
2025-08-26 07:09:25 -0600

    fix(style): adjust image picker button styles for consistency

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -3,41 +3,62 @@ import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
 import Image from 'next/image';
 
-export default function ImagePicker({label, name}){
-const [pickedImage, setPickedImage] = useState();
-const imageInput = useRef();
+export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
+    const imageInput = useRef();
 
-function handlePickClick(){
-    imageInput.current.click();
-}
-
-function handleImageChange(event){
-    const file = event.target.files[0];
-     
-    if (!file) {
-        setPickedImage(null);
-        return;
+    function handlePickClick() {
+        imageInput.current.click();
     }
 
-    const fileReader = new FileReader();
+    function handleImageChange(event) {
+        const file = event.target.files[0];
 
-    fileReader.onload = () => {
-       setPickedImage(fileReader.result);
-    };
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
 
-    fileReader.readAsDataURL(file);
+        const fileReader = new FileReader();
 
-}
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
+
+    }
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
-            <div className={classes.preview}>
-                {!pickedImage && <p>No Image picked yet.</p>}
-                {pickedImage && <Image src={pickedImage} alt='The image selected by the user' fill />}
-            </div>
             <div className={classes.controls}>
-                <input type='file' id="image" required onChange={handleImageChange} className={classes.input} ref={imageInput} accept='image/png, image/jpeg' name={name} />
-                <button className={classes.button} type="button" onClick={handlePickClick}>Pick an Image</button>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && (
+                        <Image
+                            src={pickedImage}
+                            alt="The image selected by the user."
+                            fill
+                        />
+                    )}
+                </div>
+                <input
+                    className={classes.input}
+                    type="file"
+                    id={name}
+                    accept="image/png, image/jpeg"
+                    name={name}
+                    ref={imageInput}
+                    required
+                    onChange={handleImageChange}
+                />
+                <button
+                    className={classes.button}
+                    type="button"
+                    onClick={handlePickClick}
+                >
+                    Pick an Image
+                </button>
             </div>
         </div>
     );
