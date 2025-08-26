5efadd2417e9c71b56c770c15c2b5e6fc447c3d6
Alejandro Bermúdez <mejorandro@gmail.com>
2025-08-26 06:47:20 -0600

    feat(ui): handle image file button click to open file selector

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,19 @@
 'use client';
+import { useRef } from 'react';
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({label, name}){
+const imageInput = useRef();
+
 function handlePickClick(){
-    //TODO: Funcionality of image upload!
+    imageInput.current.click();
 }
 
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
-                <input type='file' id="image" className={classes.input} accept='image/png, image/jpeg' name={name} />
+                <input type='file' id="image" className={classes.input} ref={imageInput} accept='image/png, image/jpeg' name={name} />
                 <button className={classes.button} type="button" onClick={ImagePicker}>Pick an Image</button>
             </div>
         </div>
